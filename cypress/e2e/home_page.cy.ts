d2560ded5b61bd2d7fd476ed37d7897342e6faad
alro-dev <alarogala@gmail.com>
2023-02-17 12:27:34 +0100

    few more test

--- cypress/e2e/home_page.cy.ts
+++ cypress/e2e/home_page.cy.ts
@@ -1,7 +1,27 @@
-describe('The Home Page', () => {
-    it('when click narco box, name input is narco', () => {
-      cy.visit('http://localhost:4200/dashboard') 
-      cy.contains('Narco').click(),
-      cy.get('input[placeholder="name"]').should('have.value','Narco')
-})
-  })
+describe("The Home Page", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:4200/dashboard");
+  });
+
+  it("when click narco box, name input is narco", () => {
+    cy.contains("Narco").click(),
+      cy.get('input[placeholder="name"]').should("have.value", "Narco");
+  });
+
+  it("when click narco box, id input is 12", () => {
+    cy.contains("Narco").click(),
+      cy.get("label").contains("id").parent().should("have.text", "id: 12");
+  });
+
+  it("check status code", () => {
+    cy.request('GET', "/").then(
+  (response) => {
+    expect(response.status).to.be.eql(200);
+  }
+)
+      });
+
+});
+
+
+
